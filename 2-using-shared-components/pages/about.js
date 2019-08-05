ccf2e567037e46105265b08a638923bf4baf27eb
Vincent <vince.farah@haymarket.com>
2019-08-05 12:14:50 +0100

    Learning course

--- 2-using-shared-components/pages/about.js
+++ 2-using-shared-components/pages/about.js
@@ -1,7 +1,11 @@
+import Layout from "../components/MyLayout";
+
 export default function About() {
   return (
     <div>
-      <p>This is the about page</p>
+      <Layout>
+        <p>This is the about page</p>
+      </Layout> 
     </div>
   )
 }
--- 2-using-shared-components/pages/index.js
+++ 2-using-shared-components/pages/index.js
@@ -1,12 +1,17 @@
 import Link from 'next/link'
+import Header from '../components/Header';
+import Layout from './../components/MyLayout';
 
 export default function Index() {
   return (
     <div>
+      <Header />
       <Link href="/about">
         <a>About Page</a>
       </Link>
-      <p>Hello Next.js</p>
+      <Layout>
+        <p>Hello Next.js</p>
+      </Layout>
     </div>
   )
 }
